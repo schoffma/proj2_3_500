15e2267906adc8c36f47c62b5eacfb7d26fc7bfe
schoffma <78433083+schoffma@users.noreply.github.com>
2021-02-23 18:24:21 -0500

    Add files via upload

---
+++ fun.py
@@ -0,0 +1,6 @@
+'''
+Created on Feb 18, 2021
+
+@author: schof
+'''
+print('Hello you!')
---
+++ funfunc.py
@@ -0,0 +1,11 @@
+'''
+Created on Feb 18, 2021
+
+@author: schof
+'''
+def say_hey():
+    print('Hello you!')
+    
+#calls function say_hey
+
+say_hey()    
